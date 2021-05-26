41969383aa2deca2d385150f2aba53a8b7f52073
Developr <alperen.yildirim7@gmail.com>
2021-05-26 13:46:41 +0300

    Created first utility class
    Webdriver Utilities

---
+++ com.selenium.utilities/WebdriverFactory.java
@@ -0,0 +1,43 @@
+package com.selenium.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class WebdriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+        WebDriver driver = null;
+        switch (browserType.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":                       
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "opera":
+                WebDriverManager.operadriver().setup();
+                driver = new OperaDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case "internet explorer":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            default:
+                driver = null;
+        }
+        return driver;
+
+
+    }
+}
